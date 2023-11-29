3c7bcfb8d43a7b0e170eb200535db43623d5a3bf
i200405 <149363720+i200405@users.noreply.github.com>
2023-11-29 22:30:50 +0500

    Fragments Updated

--- app/src/main/java/com/project/i200557_i200405_i200590/Frags.java
+++ app/src/main/java/com/project/i200557_i200405_i200590/Frags.java
@@ -78,3 +78,86 @@ public class Frags extends AppCompatActivity {
         return true;
     }
 }
+
+//
+//
+//
+//        import androidx.appcompat.app.AppCompatActivity;
+//        import androidx.fragment.app.Fragment;
+//        import androidx.fragment.app.FragmentManager;
+//        import androidx.fragment.app.FragmentTransaction;
+//
+//        import android.os.Bundle;
+//        import android.view.Menu;
+//        import android.widget.Toolbar;
+//
+//        import com.project.i200557_i200405_i200590.databinding.ActivityFragsBinding;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    ActivityFragsBinding binding;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        binding= ActivityFragsBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        replaceFragment(new TimelineFragment());
+//        binding.bottomNavigationView.setBackground(null);
+//
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//            int id = item.getItemId();
+//
+//            if (id == R.id.timeline) {
+//                replaceFragment(new TimelineFragment());
+//
+//                return true;
+//            }
+//            else if (id==R.id.itinerary) {
+//                replaceFragment(new ItineraryFragment());
+//                return  true;
+//            }
+//
+//            else if (id==R.id.add) {
+//                replaceFragment(new AddFragment());
+//                return  true;
+//            }
+//            else if (id==R.id.calendar) {
+//                replaceFragment(new CalendarFragment());
+//                return  true;
+//            }
+//            else if (id==R.id.map) {
+//                replaceFragment(new MapFragment());
+//
+//                return  true;
+//            }
+//            return true;
+//        });
+//    }
+//
+//    private void setSupportActionBar(Toolbar toolbar) {
+//    }
+//
+//
+//    private void replaceFragment(Fragment fragment) {
+//
+//
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.frag_linear,fragment);
+//        fragmentTransaction.commit();
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.toolbar,menu);
+//
+//        return true;
+//    }
+//
+//}
--- app/src/main/java/com/project/i200557_i200405_i200590/MainActivity.java
+++ app/src/main/java/com/project/i200557_i200405_i200590/MainActivity.java
@@ -1,14 +1,43 @@
 package com.project.i200557_i200405_i200590;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+
+    ImageButton timeline;
+    LinearLayout Frag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frags);
+        setContentView(R.layout.activity_main);
+
+        timeline = findViewById(R.id.timeline);
+        Frag = findViewById(R.id.frag_linear);
+
+        timeline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimelineFragment timelineFragment = new TimelineFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frag_linear, timelineFragment);
+                transaction.commit();
+            }
+        });
+
+
     }
+
+
 }
