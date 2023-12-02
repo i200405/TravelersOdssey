41d2701650eefbba1f1f1769d78cf3d49ef5b2be
i200405 <149363720+i200405@users.noreply.github.com>
2023-12-02 20:51:41 +0500

    FireBase Update

--- app/src/main/java/com/project/i200557_i200405_i200590/SignIn.java
+++ app/src/main/java/com/project/i200557_i200405_i200590/SignIn.java
@@ -1,17 +1,29 @@
 package com.project.i200557_i200405_i200590;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
 
     TextView forgetPass;
     Button Sign_in, Sign_up;
+    EditText editTextEmail,editTextPass;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,9 @@ public class SignIn extends AppCompatActivity {
         forgetPass = findViewById(R.id.forget_pass);
         Sign_in = findViewById(R.id.Signin);
         Sign_up = findViewById(R.id.Signup);
+        editTextEmail = findViewById(R.id.email);
+        editTextPass = findViewById(R.id.pass);
+        mAuth = FirebaseAuth.getInstance();
 
 
         forgetPass.setOnClickListener(new View.OnClickListener() {
@@ -35,8 +50,37 @@ public class SignIn extends AppCompatActivity {
         Sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignIn.this, MainActivity.class);
-                startActivity(intent);
+                String email,pass;
+                email = String.valueOf(editTextEmail.getText());
+                pass = String.valueOf(editTextPass.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(SignIn.this,"Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)){
+                    Toast.makeText(SignIn.this,"Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(SignIn.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignIn.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
             }
         });
 
@@ -47,6 +91,18 @@ public class SignIn extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            // User is signed in, redirect to another activity or perform necessary actions.
+            Intent intent = new Intent(SignIn.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Optional: Close the SignIn activity if the user is already signed in.
+        }
     }
 
 
--- app/src/main/java/com/project/i200557_i200405_i200590/SignUp.java
+++ app/src/main/java/com/project/i200557_i200405_i200590/SignUp.java
@@ -1,14 +1,80 @@
 package com.project.i200557_i200405_i200590;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText editTextEmail,editTextPass,username;
+    Button signup;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextEmail = findViewById(R.id.etsemail);
+        editTextPass = findViewById(R.id.etspass);
+        username = findViewById(R.id.etname);
+        signup = findViewById(R.id.SignupS);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uname,email,pass;
+                uname = String.valueOf(username.getText());
+                email = String.valueOf(editTextEmail.getText());
+                pass = String.valueOf(editTextPass.getText());
+                mAuth = FirebaseAuth.getInstance();
+
+                if (TextUtils.isEmpty(uname)){
+                    Toast.makeText(SignUp.this,"Enter Username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(SignUp.this,"Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)){
+                    Toast.makeText(SignUp.this,"Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(SignUp.this, "Authenticated.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignUp.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
